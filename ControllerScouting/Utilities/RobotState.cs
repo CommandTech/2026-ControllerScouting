4fba95114668460523170e95ba6d3c42891baa92
Ruchir-Kafle <ruchirkafle@gmail.com>
2026-01-26 18:24:21 -0500

    update some robot enums

--- ControllerScouting/Utilities/RobotState.cs
+++ ControllerScouting/Utilities/RobotState.cs
@@ -168,9 +168,9 @@ namespace ControllerScouting.Utilities
         public bool DisplayClimbSuccess = false;
         public enum STARTING_LOCATION { Far_Trench, Far_Bump, Hub, Near_Bump, Near_Trench }
         public enum LADDER_LOCATION { Outpost, Center, Depot };
-        public enum DEFENSE_STRATEGY { None, Trench, Bump, Neutral_Zone, Pushing };
-        public enum AVOIDANCE_STRATEGY { None, Trench, Bump, Neutral_Zone, Pushing };
-        public enum STRATEGY { None, Shooting, Feeding };
+        public enum DEFENSE_STRATEGY { None, Lane, Neutral_Zone, Hub, Pinning, Counter };
+        public enum AVOIDANCE_STRATEGY { None, Lane, Neutral_Zone, Hub, Pinning, Counter };
+        public enum STRATEGY { None, Shooting, Feeding, Defense, Climb };
         public enum CLIMB_LEVEL { No_Climb, L1, L2, L3 };
 
 
