8bd1d2b8dbb8f582022a4d95b5dadc5e28425255
CommandTech <lpbachman@wpi.edu>
2026-03-27 12:21:14 -0400

    Removed Auto Climb and End State from controls, grabbing data from TBA instead

--- ControllerScouting/Screens/BaseScreen.cs
+++ ControllerScouting/Screens/BaseScreen.cs
@@ -717,7 +717,8 @@ namespace ControllerScouting.Screens
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position2Value", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position3", true)[0]).Visible = true;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position3Value", true)[0]).Visible = false;
-            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Visible = true;
+            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Visible = false;
+            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position6", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position7", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position8", true)[0]).Visible = false;
@@ -733,9 +734,6 @@ namespace ControllerScouting.Screens
 
                 if (robot.AUTO)
                 {
-                    ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Text = "Climb: ";
-                    ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).Visible = true;
-
                     if (robot.GetAutoClimb() == RobotState.BOOLEAN.Z)
                     {
                         ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).ForeColor = Color.Yellow;
@@ -797,7 +795,8 @@ namespace ControllerScouting.Screens
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position2Value", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position3", true)[0]).Visible = true;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position3Value", true)[0]).Visible = false;
-            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Visible = true;
+            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Visible = false;
+            ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position6", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position7", true)[0]).Visible = false;
             ((Label)this.Controls.Find($"lbl{ScouterBox}Position8", true)[0]).Visible = false;
@@ -812,9 +811,6 @@ namespace ControllerScouting.Screens
                 ((Label)this.Controls.Find($"lbl{ScouterBox}Position2", true)[0]).Text = "Shooting Timer: " + robot.FuelShootingTime.TotalSeconds.ToString("F2");
                 if (robot.AUTO)
                 {
-                    ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).Visible = true;
-                    ((Label)this.Controls.Find($"lbl{ScouterBox}Position4", true)[0]).Text = "Climb: ";
-                    ((Label)this.Controls.Find($"lbl{ScouterBox}Position4Value", true)[0]).Visible = true;
 
                     if (robot.GetAutoClimb() == RobotState.BOOLEAN.Z)
                     {
